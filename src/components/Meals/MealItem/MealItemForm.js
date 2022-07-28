284a80ec338323c43c04860b53481f43d8205afa
heonyDev <dlxogjsdut@naver.com>
2022-07-28 15:15:09 +0900

    componets 기본 구축완료

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,6 +1,23 @@
+import classes from './MealItemForm.module.css';
+import Input from '../../UI/Input';
 
-const MealItemForm = () =>{
-    return <input/>
-}
+const MealItemForm = () => {
+  return (
+    <form className={classes.form}>
+      <Input
+        label="수량"
+        input={{
+          type: 'number',
+          id: 'amount',
+          min: '1',
+          max: '5',
+          setp: '1',
+          defaultValue: '1',
+        }}
+      />
+      <button> 카트 담기 </button>
+    </form>
+  );
+};
 
-export default MealItemForm
+export default MealItemForm;
